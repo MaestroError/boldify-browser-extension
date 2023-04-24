626bded239d259bf517956ee6d5f6837c3661c91
maestroerror <revaz.gh@gmail.com>
2023-04-24 13:17:43 +0400

    Turn off on second click

--- source/background.js
+++ source/background.js
@@ -2,10 +2,20 @@
 import './options-storage.js';
 
 const extension = typeof browser !== 'undefined' ? browser : chrome;
+const tabStates = new Map();
 
-chrome.action.onClicked.addListener(async (tab) => {
-  await chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['content.js']
-  });
+extension.action.onClicked.addListener(async (tab) => {
+  const tabId = tab.id;
+  const isEnabled = tabStates.get(tabId);
+
+  if (isEnabled) {
+    await extension.tabs.reload(tabId);
+    tabStates.set(tabId, false);
+  } else {
+    await extension.scripting.executeScript({
+      target: { tabId: tab.id },
+      files: ['content.js']
+    });
+    tabStates.set(tabId, true);
+  }
 });
